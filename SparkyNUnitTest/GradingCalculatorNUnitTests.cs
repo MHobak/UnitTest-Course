c628b38e6a30ec169d08c1bd3fee97da0b84d8d1
Maiko <miguel.hobak@outlook.com>
2023-10-09 17:26:55 -0600

    Refactor assignment tests

--- SparkyNUnitTest/GradingCalculatorNUnitTests.cs
+++ SparkyNUnitTest/GradingCalculatorNUnitTests.cs
@@ -18,26 +18,6 @@ namespace SparkyNUnitTest
         ///Score 95, Attendance 90 -> 'B'
         ///Score 95, Attendance 90 -> 'C'
         ///Score 95, Attendance 90 -> 'B'
-        /// </summary>
-        /// <param name="score">Score ponts</param>
-        /// <param name="attendancePercentage">Attandance percentage</param>
-        /// <param name="result">Final Note</param>
-        [Test]
-        [TestCase(95, 90, "A")]
-        [TestCase(85, 90, "B")]
-        [TestCase(65, 90, "C")]
-        [TestCase(95, 65, "B")]
-        public void Grading_GetPasssingNote(int score, int attendancePercentage, string result) 
-        {
-            //Act
-            grading.Score = score;
-            grading.AttendancePercentage = attendancePercentage;
-
-            //Assert
-            Assert.That(grading.GetGrade(), Is.EqualTo(result));
-        }
-
-        /// <summary>
         ///Score 95, Attendance 55 -> 'F'
         ///Score 65, Attendance 55 -> 'F'
         ///Score 50, Attendance 90 -> 'F'
@@ -46,17 +26,21 @@ namespace SparkyNUnitTest
         /// <param name="attendancePercentage">Attandance percentage</param>
         /// <param name="result">Final Note</param>
         [Test]
-        [TestCase(95, 55, "F")]
-        [TestCase(65, 55, "F")]
-        [TestCase(50, 90, "F")]
-        public void Grading_GetFailedNote(int score, int attendancePercentage, string result)
+        [TestCase(95, 90, ExpectedResult = "A")]
+        [TestCase(85, 90, ExpectedResult = "B")]
+        [TestCase(65, 90, ExpectedResult = "C")]
+        [TestCase(95, 65, ExpectedResult = "B")]
+        [TestCase(95, 55, ExpectedResult = "F")]
+        [TestCase(65, 55, ExpectedResult = "F")]
+        [TestCase(50, 90, ExpectedResult = "F")]
+        public string GradingCalc_AllGradeLogicalScenarios_GradeOutput(int score, int attendancePercentage) 
         {
             //Act
             grading.Score = score;
             grading.AttendancePercentage = attendancePercentage;
 
             //Assert
-            Assert.That(grading.GetGrade(), Is.EqualTo(result));
+            return grading.GetGrade();
         }
     }
 }
